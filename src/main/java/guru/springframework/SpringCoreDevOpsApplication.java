c2854360b6424cf86a14aac2531cbe1a8b35d152
steveshvets <steve.shvets@gmail.com>
2020-03-29 16:55:02 -0400

    first message

---
+++ src/main/java/guru/springframework/SpringCoreDevOpsApplication.java
@@ -0,0 +1,14 @@
+package guru.springframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAutoConfiguration
+public class SpringCoreDevOpsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCoreDevOpsApplication.class, args);
+	}
+}
